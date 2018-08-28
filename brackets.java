f6ee7149d65ec8d6a3674e5f80cfac004290ff79
mrybhatia <36405508+mrybhatia@users.noreply.github.com>
2018-08-28 18:46:48 +0530

    Add files via upload

---
+++ brackets.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class main{
+	public static void main(String[] argv){
+	    Scanner sc = new Scanner(System.in);
+	    String bracket = sc.next();
+	    char[] a = bracket.toCharArray();
+	    int count = 0;
+	    for (int i=0;i<a.length-1;i++) {
+	    	for (int j=i+1;j<a.length;j++){
+	    		if(a[i] == '[' && a[j] ==']'){
+	    			count += 1;
+	    			a[i] = '0';
+	    			a[j] = '0';
+	    			continue;
+	    		}
+	    	}
+	    }
+	    System.out.println(count);
+	}
+}
